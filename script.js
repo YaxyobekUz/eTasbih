b36161e33f4a669a9b4892a5574465b49aab57b1
mryaxyobek <pubgn9642@gmail.com>
2023-06-01 10:54:16 +0500

    added js codes

---
+++ script.js
@@ -0,0 +1,15 @@
+const elScreen = document.querySelector('#screen');
+const elReset = document.querySelector('#reset');
+const elCounter = document.querySelector('#counter');
+
+// btn
+elCounter.addEventListener('click', function(){
+    elScreen.textContent = +elScreen.textContent+1
+})
+
+// reset 
+elReset.addEventListener('click', function(){
+    // elSelect.value = 'Zikrlar'
+    // elZikr.textContent = "ltimos aytmoqchi bo'lgan zikrni tanlang :("
+    elScreen.textContent = '0'
+})
